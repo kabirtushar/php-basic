292b413d17859858a7e4cefdd9764399c004bfb2
kabirtushar <tushar.kabir@yahoo.com>
2016-04-27 20:48:36 +0600

    to collect data

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>get / post / request </title>
+</head>
+<body>
+    
+       
+       
+    <form action="result.php" method="POST">
+        <label for="name">name</label>
+        <input type="text"  placeholder="name" name="name" id="name">
+        
+        <br>
+        <label for="contact">marks</label>
+        <input type="text"  placeholder="marks" name="marks" id="contact">
+        <input type="submit" value="submit" name="form1">
+        
+    </form>
+    
+    
+  
+    
+    
+</body>
+</html>
